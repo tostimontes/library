24ea031eb86a7f4f6a4175f54a51d0a29df8c201
tostimontes <147332922+tostimontes@users.noreply.github.com>
2024-01-05 21:09:14 +0100

    feat: book functionality COMPLETE

--- script.js
+++ script.js
@@ -3,22 +3,26 @@ function Book(title, author, pages, read) {
   this.author = author;
   this.pages = pages;
   this.read = read;
-
-  };
+}
 // Add sorting functionality
 
+const myLibrary = [];
 const addBookButton = document.getElementById("add_new_book");
 const dialog = document.querySelector("#book_form_dialog");
 const form = document.querySelector("#book_form");
 const closeDialogButton = document.querySelector("#close_dialog");
 const submitBook = document.querySelector("#form_submit");
+const libraryUI = document.querySelector(".library");
 closeDialogButton.addEventListener("click", () => {
   dialog.close();
 });
 
-
-form.addEventListener('submit', function(event) {
+form.addEventListener("submit", function (event) {
   event.preventDefault();
+});
+
+addBookButton.addEventListener("click", () => {
+  dialog.showModal();
 });
 
 submitBook.addEventListener("click", () => {
@@ -27,40 +31,52 @@ submitBook.addEventListener("click", () => {
   const newPages = document.getElementById("pages").value;
   let newRead = document.getElementById("read").checked;
 
-  createBookCard(newTitle, newAuthor, newPages, newRead);
-})
+  const newBook = new Book(newTitle, newAuthor, newPages, newRead);
+  myLibrary.push(newBook);
+  createBookCard(newBook);
+  clearForm();
+  dialog.close();
+});
 // HTML Book Card creation
-function createBookCard(title, author, pages, read) {
-  const newBook = new Book(title, author, pages, read);
+function createBookCard(book) {
   const bookCard = document.createElement("div");
   bookCard.classList.add("card");
   const bookTitle = document.createElement("h2");
   bookTitle.classList.add("book_title");
-  bookTitle.textContent = newBook.title;
+  bookTitle.textContent = book.title;
   const bookAuthor = document.createElement("h3");
   bookAuthor.classList.add("book_author");
-  bookAuthor.textContent = newBook.author;
+  bookAuthor.textContent = book.author;
   const bookPages = document.createElement("p");
   bookPages.classList.add("book_pages");
-  bookPages.textContent = newBook.pages;
+  bookPages.textContent = `${book.pages} pages`;
   const bookRead = document.createElement("button");
   bookRead.classList.add("read_status");
-  bookRead.setAttribute("value", newBook.read)
-  bookRead.textContent = newBook.read;
-  bookCard.append(bookTitle, bookAuthor, bookPages, bookRead)
-  console.log(bookCard);
+  bookRead.setAttribute("value", book.read);
+  book.read
+    ? (bookRead.textContent = "Read")
+    : (bookRead.textContent = "Not read yet");
+  bookCard.append(bookTitle, bookAuthor, bookPages, bookRead);
+  updateLibraryUI(bookCard);
 }
 
+function updateLibraryUI(card) {
+  libraryUI.appendChild(card);
+}
+
+function clearForm() {
+  document.querySelector("#title").value = "";
+  document.querySelector("#author").value = "";
+  document.querySelector("#pages").value = null;
+  document.querySelector("#read").checked = false;
+}
 
 const exampleBook = {
-    title: "Example Title",
-    author: "Author Smith",
-    pages: 295,
-    read: false
-}
-
-const myLibrary = [];
-
+  title: "Example Title",
+  author: "Author Smith",
+  pages: 295,
+  read: false,
+};
 
 // Loop through library function
 
@@ -68,6 +84,3 @@ const myLibrary = [];
 // Card img automatically search for google images and return first image as card background
 
 // Remove, edit and un/read toggle buttons within cards
-
-
-
