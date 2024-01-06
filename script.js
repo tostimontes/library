c043e0ec79f842f1a668abd66a6b5a2ed5658677
tostimontes <147332922+tostimontes@users.noreply.github.com>
2024-01-06 20:06:51 +0100

    feat: edit functionality COMPLETE
    - Added nav link functionality after edit
    - Book array and titles array updated

--- script.js
+++ script.js
@@ -6,11 +6,16 @@ function Book(title, author, pages, read) {
 }
 // Add sorting functionality
 let editMode = false;
+let bookInEdition;
+let cardInEdition;
+let bookTitleInEdition;
+let navLinkInEdition;
 const myLibraryArray = [];
 const libraryTitleNames = [];
 const addBookButton = document.getElementById("add_new_book");
 const dialog = document.querySelector("#book_form_dialog");
 const editDialog = document.querySelector("#edit_dialog");
+const submitEdit = document.querySelector("#edit_submit");
 const form = document.querySelector("#book_form");
 const closeDialogButton = document.querySelector("#close_dialog");
 const submitBook = document.querySelector("#form_submit");
@@ -18,16 +23,35 @@ const libraryUI = document.querySelector(".library");
 const bookList = document.querySelector(".book_list");
 
 closeDialogButton.addEventListener("click", () => {
-  clearForm()
+  clearForm();
   dialog.close();
 });
 
 form.addEventListener("submit", function (event) {
   event.preventDefault();
 });
-
+// add book listener
 addBookButton.addEventListener("click", () => {
   dialog.showModal();
+});
+
+// edit button listener
+libraryUI.addEventListener("click", (e) => {
+  const editDiv = e.target.closest(".edit_button");
+  if (editDiv) {
+    editDialog.showModal();
+    fillEditDialog(editDiv.parentElement);
+    bookInEdition = myLibraryArray.find(
+      (book) => book.title === editDiv.parentElement.dataset.value
+    );
+    bookTitleInEdition = libraryTitleNames.find(
+      (title) => title === editDiv.parentElement.dataset.value
+    );
+    cardInEdition = editDiv.parentElement;
+    navLinkInEdition = bookList.querySelector(
+      `a[href="#${cardInEdition.getAttribute("id")}"`
+    );
+  }
 });
 
 document.addEventListener("keydown", (e) => {
@@ -37,18 +61,14 @@ document.addEventListener("keydown", (e) => {
   }
 });
 
-
 libraryUI.addEventListener("click", (e) => {
   if (e.target.matches(".close_button")) {
     removeBookFromLibrary(e.target.parentElement);
-  } 
+  }
 });
 
+// new book submit
 submitBook.addEventListener("click", () => {
-  if (editMode) {
-    
-  }
-
   const newTitle = document.getElementById("title").value;
   const newAuthor = document.getElementById("author").value;
   const newPages = document.getElementById("pages").value;
@@ -62,18 +82,13 @@ submitBook.addEventListener("click", () => {
   dialog.close();
 });
 
-// edit button listener
-// TODO: separate edit dialog
-libraryUI.addEventListener("click", (e) => {
-  const editDiv = e.target.closest(".edit_button");
-  if (editDiv) {
-    editMode = true;
-    editDiv.parentElement.classList.add("editing");
-    editBook(editDiv.parentElement);
-    editDiv.parentElement.classList.remove("editing");
-    editMode = false;
-  }
-})
+submitEdit.addEventListener("click", () => {
+  // Change the array index object and then apply changes to the card
+  editBookCard(cardInEdition);
+
+  clearEditForm();
+  editDialog.close()
+});
 // HTML Book Card creation
 function createBookCard(book) {
   const bookCard = document.createElement("div");
@@ -82,7 +97,7 @@ function createBookCard(book) {
   bookCard.setAttribute("data-value", book.title);
   const removeButton = document.createElement("button");
   removeButton.classList.add("close_button");
-  removeButton.setAttribute("title", "Remove book from library")
+  removeButton.setAttribute("title", "Remove book from library");
   removeButton.textContent = "X";
   const editButton = document.createElement("button");
   editButton.classList.add("edit_button");
@@ -96,25 +111,56 @@ function createBookCard(book) {
   bookAuthor.textContent = book.author;
   const bookPages = document.createElement("p");
   bookPages.classList.add("book_pages");
-  bookPages.setAttribute("data-value", book.pages)
+  bookPages.setAttribute("data-value", book.pages);
   bookPages.textContent = `${book.pages} pages`;
   const bookRead = document.createElement("button");
   bookRead.classList.add("read_status");
   bookRead.setAttribute("data-value", book.read);
   book.read
-    ? (bookRead.textContent = "Read")
-    : (bookRead.textContent = "Not read yet");
-  bookCard.append(removeButton, editButton, bookTitle, bookAuthor, bookPages, bookRead);
-  addBookToLibrary(bookCard);
+  ? (bookRead.textContent = "Read")
+  : (bookRead.textContent = "Not read yet");
+  bookCard.append(
+    removeButton,
+    editButton,
+    bookTitle,
+    bookAuthor,
+    bookPages,
+    bookRead
+    );
+    addBookToLibrary(bookCard);
+  }
+  
+  function editBookCard(card) {
+    card.dataset.value = editDialog.querySelector("#edit_title").value;
+    bookTitleInEdition = editDialog.querySelector("#edit_title").value;
+    bookInEdition.title = editDialog.querySelector("#edit_title").value;
+    bookInEdition.author = editDialog.querySelector("#edit_author").value;
+    bookInEdition.pages = editDialog.querySelector("#edit_pages").value;
+    bookInEdition.read = editDialog.querySelector("#edit_read").checked;
+    card.setAttribute("href", `#${bookInEdition.title.split(" ").join("")}`);
+    navLinkInEdition.setAttribute("id", bookInEdition.title.split(" ").join(""));
+    navLinkInEdition.textContent = bookInEdition.title;
+    card.querySelector(".book_title").textContent = bookInEdition.title;
+    card.querySelector(".book_author").textContent = bookInEdition.author;
+    card.querySelector(".book_pages").dataset.value = bookInEdition.pages;
+    card.querySelector(".book_pages").textContent =
+      `${bookInEdition.pages} pages`;
+    card.querySelector(".read_status").dataset.value = bookInEdition.read;
+    card.querySelector(".read_status").textContent = bookInEdition.title;
+    bookInEdition.read
+      ? (card.querySelector(".read_status").textContent = "Read")
+      : (card.querySelector(".read_status").textContent =
+          "Not read yet");
 }
-
 function addBookToLibrary(card) {
   libraryUI.appendChild(card);
   addNavLink(card);
 }
 
 function removeBookFromLibrary(card) {
-  const indexForRemoval = myLibraryArray.findIndex((book) => book.title === card.dataset.value);
+  const indexForRemoval = myLibraryArray.findIndex(
+    (book) => book.title === card.dataset.value
+  );
   myLibraryArray.splice(indexForRemoval, 1);
   libraryTitleNames.splice(indexForRemoval, 1);
   libraryUI.removeChild(card);
@@ -136,20 +182,24 @@ function addNavLink(card) {
 
 function removeNavLink(card) {
   const linkToRemove = bookList.querySelector(
-    `a[href="#${card.dataset.value}"`
+    `a[href="#${card.getAttribute("id")}"`
+    
   );
-  
+
   if (linkToRemove) {
     linkToRemove.parentElement.remove();
-  };
+  }
 }
 
-function editBook(card) {
-  dialog.showModal();
-  dialog.querySelector("#title").value = card.querySelector(".book_title").textContent;
-  dialog.querySelector("#author").value = card.querySelector(".book_author").textContent;
-  dialog.querySelector("#pages").value = card.querySelector(".book_pages").dataset.value;
-  card.querySelector(".read_status").dataset.value === "true" ? dialog.querySelector("#read").checked = true: dialog.querySelector("#read").checked = false;
+function fillEditDialog(card) {
+  editDialog.querySelector("#edit_title").value = card.querySelector(".book_title").textContent;
+  editDialog.querySelector("#edit_author").value =
+    card.querySelector(".book_author").textContent;
+  editDialog.querySelector("#edit_pages").value =
+    card.querySelector(".book_pages").dataset.value;
+  card.querySelector(".read_status").dataset.value === "true"
+    ? (editDialog.querySelector("#edit_read").checked = true)
+    : (editDialog.querySelector("#edit_read").checked = false);
 }
 
 // TODO: edit button that pops dialog with same information, should rewrite object in library
@@ -166,13 +216,19 @@ function clearForm() {
   document.querySelector("#read").checked = false;
 }
 
+function clearEditForm() {
+  document.querySelector("#edit_title").value = "";
+  document.querySelector("#edit_author").value = "";
+  document.querySelector("#edit_pages").value = null;
+  document.querySelector("#edit_read").checked = false;
+}
+
 const exampleBook = {
   title: "Example Title",
   author: "Author Smith",
   pages: 295,
   read: false,
 };
-
 
 // Card img automatically search for google images and return first image as card background
 
