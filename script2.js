342ffafc209c8e6c4f6e0af6db9fc1fff142c577
tostimontes <147332922+tostimontes@users.noreply.github.com>
2024-01-08 15:12:10 +0100

    feat: script2 fully functional EXCEPT(see below)
    - editing resets all filters
    - read status toggle button functionality missing
    - automatic navigation and author dropdown sorting missing

--- script2.js
+++ script2.js
@@ -1,3 +1,8 @@
+// EDITION VARIABLES
+let bookInEdition;
+let bookInEditionIndex;
+let editMode = false;
+
 function Book(title = "Unknown", author = "Unknown", pages = 0, read = false) {
   this.title = title;
   this.author = author;
@@ -8,114 +13,25 @@ function Book(title = "Unknown", author = "Unknown", pages = 0, read = false) {
 function Library() {
   this.books = [];
 
-  Library.prototype.addNewBook = (title, author, pages, read) => {
+  this.addNewBook = (title, author, pages, read) => {
     if (editMode) {
-      removeBook(bookInEditionIndex);
+        // TODO: editing fine, just add automatic sort to navlinks and authordropdown in updateUI 
+      this.removeBook(bookInEditionIndex);
     }
     const newBook = new Book(title, author, pages, read);
-    this.push(newBook);
-    this.updateUI();
+    this.books.push(newBook);
+    updateUI(this.books);
     editMode = false;
   };
 
-  Library.prototype.createBookCard = (book) => {
-    const bookCard = document.createElement("div");
-    bookCard.classList.add("card");
-    bookCard.setAttribute("id", book.title.split(" ").join(""));
-    bookCard.setAttribute("data-value", book.title);
-    const removeButton = document.createElement("button");
-    removeButton.classList.add("close_button");
-    removeButton.setAttribute("title", "Remove book from library");
-    removeButton.textContent = "X";
-    const editButton = document.createElement("button");
-    editButton.classList.add("edit_button");
-    editButton.setAttribute("title", "Edit book info");
-    editButton.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><title>book-edit</title><path d="M19.39 10.74L11 19.13V22H6C4.89 22 4 21.11 4 20V4C4 2.9 4.89 2 6 2H7V9L9.5 7.5L12 9V2H18C19.1 2 20 2.89 20 4V10.3C19.78 10.42 19.57 10.56 19.39 10.74M13 19.96V22H15.04L21.17 15.88L19.13 13.83L13 19.96M22.85 13.47L21.53 12.15C21.33 11.95 21 11.95 20.81 12.15L19.83 13.13L21.87 15.17L22.85 14.19C23.05 14 23.05 13.67 22.85 13.47Z" /></svg>`;
-    const bookTitle = document.createElement("h2");
-    bookTitle.classList.add("book_title");
-    bookTitle.textContent = book.title;
-    const bookAuthor = document.createElement("h3");
-    bookAuthor.classList.add("book_author");
-    bookAuthor.textContent = book.author;
-    if (
-      !authorFilterDropdown.contains(
-        authorFilterDropdown.querySelector(`option[value="${book.author}"]`)
-      )
-    ) {
-      const authorOption = document.createElement("option");
-      authorOption.textContent = book.author;
-      authorOption.setAttribute("value", `${book.author}`);
-      authorFilterDropdown.appendChild(authorOption);
-    }
-    const bookPages = document.createElement("p");
-    bookPages.classList.add("book_pages");
-    bookPages.setAttribute("data-value", book.pages);
-    bookPages.textContent = `${book.pages} pages`;
-    const bookRead = document.createElement("button");
-    bookRead.classList.add("read_status");
-    bookRead.setAttribute("data-value", book.read);
-    bookRead.setAttribute("title", "Toggle read status");
-    book.read
-      ? (bookRead.textContent = "Read")
-      : (bookRead.textContent = "Not read yet");
-    bookCard.append(
-      removeButton,
-      editButton,
-      bookTitle,
-      bookAuthor,
-      bookPages,
-      bookRead
-    );
-    libraryUI.appendChild(bookCard);
-    adjustFontSizeToFit(bookCard.querySelector(".book_title"));
-
-    // Create nav link
-    const listItem = document.createElement("li");
-    const anchor = document.createElement("a");
-    anchor.textContent = bookCard.children[2].textContent;
-    anchor.setAttribute(
-      "href",
-      `#${bookCard.children[2].textContent.split(" ").join("")}`
-    );
-    anchor.classList.add("nav_link");
-    listItem.appendChild(anchor);
-    bookList.appendChild(listItem);
-
-    // listener on save button
-    // form input info to Book constructor and .push to library
-    // WHENEVER A BOOK IS PUSHED ==>>> updateUI
-    // erase form
-  };
-
-  Library.prototype.removeBook = (index) => {
-    this.splice(index, 1);
-
-    // remove nav links and the update
-    // updateUI creating 1. All books, 2. All nav links, 3. All author options IF not duplicated
-    // updateUI ALWAYS sets displays and checks for filters before rendering UI
-  };
-
-  Library.prototype.updateUI = () => {
-    libraryUI.innerHTML = "";
-    bookList.innerHTML = "";
-    authorFilterDropdown.innerHTML = "";
-    const allAuthorsOption = document.createElement("option");
-    allAuthorsOption.setAttribute("data-value", "All authors");
-    allAuthorsOption.textContent = "All authors";
-    authorFilterDropdown.appendChild(allAuthorsOption);
-    for (const book of this) {
-      createBookCard(book);
-    }
-    checkFilters();
+  this.removeBook = (index) => {
+    this.books.splice(index, 1);
+    updateUI(this.books);
   };
 }
 // GLOBAL VARIABLES
 const myLibrary = new Library();
 
-// EDITION VARIABLES
-let editMode = false;
-let bookInEdition;
-let bookInEditionIndex;
 
 // SELECTORS
 
@@ -167,40 +83,154 @@ libraryUI.addEventListener("click", (e) => {
   // fetch book reference from library and save in case there's edition
   if (editDiv) {
     editMode = true;
-    bookInEdition = myLibrary.find(
-      (book) => book.title === editDiv.dataset.value
+    bookInEdition = myLibrary.books.find(
+      (book) => book.title === cardInEdition.dataset.value
     );
-    bookInEditionIndex = myLibrary.findIndex(
-      (book) => book.title === editDiv.dataset.value
+    bookInEditionIndex = myLibrary.books.findIndex(
+      (book) => book.title === cardInEdition.dataset.value
     );
+
+    dialog.showModal();
+
+    // populate form with old values
+    document.getElementById("title").value =
+      cardInEdition.querySelector(".book_title").textContent;
+    document.getElementById("author").value =
+      cardInEdition.querySelector(".book_author").textContent;
+    document.getElementById("pages").value =
+      cardInEdition.querySelector(".book_pages").dataset.value;
+    cardInEdition.querySelector(".read_status").dataset.value === "true"
+      ? (document.getElementById("read").checked = true)
+      : (document.getElementById("read").checked = false);
   }
-
-  dialog.showModal();
-
-  // populate form with old values
-  document.getElementById("title").value =
-    card.querySelector(".book_title").textContent;
-  document.getElementById("author").value =
-    card.querySelector(".book_author").textContent;
-  document.getElementById("pages").value =
-    card.querySelector(".book_pages").dataset.value;
-  card.querySelector(".read_status").dataset.value === "true"
-    ? (document.getElementById("read").checked = true)
-    : (document.getElementById("read").checked = false);
 });
 
 // REMOVE button listener
 libraryUI.addEventListener("click", (e) => {
   if (e.target.matches(".close_button")) {
-    const indexForRemoval = myLibrary.findIndex(
+    const indexForRemoval = myLibrary.books.findIndex(
       (book) => book.title === e.target.parentElement.dataset.value
     );
     myLibrary.removeBook(indexForRemoval);
   }
 });
 
+// FILTER listener
+filtersPanel.addEventListener("change", (e) => {
+  if (e.target.tagName === "INPUT" || e.target.tagName === "SELECT") {
+    checkFilters();
+  }
+});
+
+resetFiltersButton.addEventListener("click", resetFilters);
+
+closeDialogButton.addEventListener("click", () => {
+  clearForm();
+  dialog.close();
+});
+
+document.addEventListener("keydown", (e) => {
+  if (e.key === "Escape" && dialog.hasAttribute("open")) {
+    clearForm();
+    dialog.close();
+  }
+});
+
+// TODO: un/read toggle
+
 // GLOBAL FUNCTIONS
 
+function updateUI(library) {
+  libraryUI.innerHTML = "";
+  bookList.innerHTML = "";
+//   TODO: Record filtered status for below vvvvvvv
+  authorFilterDropdown.innerHTML = "";
+  const allAuthorsOption = document.createElement("option");
+  allAuthorsOption.setAttribute("data-value", "All authors");
+  allAuthorsOption.textContent = "All authors";
+  authorFilterDropdown.appendChild(allAuthorsOption);
+  for (const book of library) {
+      createBookCard(book);
+    }
+    // ^^^^^^^ Fetch and append in-place filters before repopulating UI, so that editions do not reset filters
+  checkFilters();
+}
+
+function createBookCard(book) {
+  const bookCard = document.createElement("div");
+  bookCard.classList.add("card");
+  bookCard.setAttribute("id", book.title.split(" ").join(""));
+  bookCard.setAttribute("data-value", book.title);
+  const removeButton = document.createElement("button");
+  removeButton.classList.add("close_button");
+  removeButton.setAttribute("title", "Remove book from library");
+  removeButton.textContent = "X";
+  const editButton = document.createElement("button");
+  editButton.classList.add("edit_button");
+  editButton.setAttribute("title", "Edit book info");
+  editButton.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><title>book-edit</title><path d="M19.39 10.74L11 19.13V22H6C4.89 22 4 21.11 4 20V4C4 2.9 4.89 2 6 2H7V9L9.5 7.5L12 9V2H18C19.1 2 20 2.89 20 4V10.3C19.78 10.42 19.57 10.56 19.39 10.74M13 19.96V22H15.04L21.17 15.88L19.13 13.83L13 19.96M22.85 13.47L21.53 12.15C21.33 11.95 21 11.95 20.81 12.15L19.83 13.13L21.87 15.17L22.85 14.19C23.05 14 23.05 13.67 22.85 13.47Z" /></svg>`;
+  const bookTitle = document.createElement("h2");
+  bookTitle.classList.add("book_title");
+  bookTitle.textContent = book.title;
+  const bookAuthor = document.createElement("h3");
+  bookAuthor.classList.add("book_author");
+  bookAuthor.textContent = book.author;
+  if (
+    !authorFilterDropdown.contains(
+      authorFilterDropdown.querySelector(`option[value="${book.author}"]`)
+    )
+  ) {
+    const authorOption = document.createElement("option");
+    authorOption.textContent = book.author;
+    authorOption.setAttribute("value", `${book.author}`);
+    authorFilterDropdown.appendChild(authorOption);
+  }
+  const bookPages = document.createElement("p");
+  bookPages.classList.add("book_pages");
+  bookPages.setAttribute("data-value", book.pages);
+  bookPages.textContent = `${book.pages} pages`;
+  const bookRead = document.createElement("button");
+  bookRead.classList.add("read_status");
+  bookRead.setAttribute("data-value", book.read);
+  bookRead.setAttribute("title", "Toggle read status");
+  book.read
+    ? (bookRead.textContent = "Read")
+    : (bookRead.textContent = "Not read yet");
+  bookCard.append(
+    removeButton,
+    editButton,
+    bookTitle,
+    bookAuthor,
+    bookPages,
+    bookRead
+  );
+  libraryUI.appendChild(bookCard);
+  adjustFontSizeToFit(bookCard.querySelector(".book_title"));
+
+  // Create nav link
+  const listItem = document.createElement("li");
+  const anchor = document.createElement("a");
+  anchor.textContent = bookCard.children[2].textContent;
+  anchor.setAttribute(
+    "href",
+    `#${bookCard.children[2].textContent.split(" ").join("")}`
+  );
+  anchor.classList.add("nav_link");
+  listItem.appendChild(anchor);
+  bookList.appendChild(listItem);
+
+  // listener on save button
+  // form input info to Book constructor and .push to library
+  // WHENEVER A BOOK IS PUSHED ==>>> updateUI
+  // erase form
+}
+
+function clearForm() {
+  document.querySelector("#title").value = "";
+  document.querySelector("#author").value = "";
+  document.querySelector("#pages").value = null;
+  document.querySelector("#read").checked = false;
+}
 function checkFilters() {
   const cardsInDisplay = document.querySelectorAll(".card");
   const cardsArray = Array.from(cardsInDisplay);
@@ -278,12 +308,12 @@ function checkFilters() {
       card.style.display = "none";
       bookList.querySelector(
         `a[href="#${card.getAttribute("id")}"]`
-      ).style.display = "none";
+      ).parentElement.style.display = "none";
     } else {
       card.style.display = "block";
       bookList.querySelector(
         `a[href="#${card.getAttribute("id")}"]`
-      ).style.display = "block";
+      ).parentElement.style.display = "block";
     }
   }
 }
@@ -292,7 +322,7 @@ function resetFilters() {
   authorFilterDropdown.value = "All authors";
   minInput.value = "";
   maxInput.value = "";
-  readFilter = "default";
+  readFilter.value = "default";
   checkFilters();
 }
 
@@ -308,7 +338,6 @@ function adjustFontSizeToFit(container) {
     contentWidth = container.scrollWidth;
   }
 }
-
 
 // Example initialization
 
@@ -455,9 +484,9 @@ const exampleArray = [
 ];
 
 function initializeExampleUI(array) {
-    for (const book of array) {
-        myLibrary.addNewBook(book.title, book.author, book.pages, book.read);
-    }
+  for (const book of array) {
+    myLibrary.addNewBook(book.title, book.author, book.pages, book.read);
+  }
 }
 
 initializeExampleUI(exampleArray);
